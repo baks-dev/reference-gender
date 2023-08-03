1de08bb441c48e168bf7b4af5c7fe0449398b554
kingstar <admin@baks.dev>
2023-08-04 00:48:56 +0300

    Baks Development

--- Resources/config/twig.php
+++ Resources/config/twig.php
@@ -2,13 +2,8 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use BaksDev\Reference\Color\Twig\ColorExtension;
 use Symfony\Config\TwigConfig;
 
 return static function(TwigConfig $config) {
 	$config->path(__DIR__.'/../view', 'Gender');
 };
-
-
-
-
