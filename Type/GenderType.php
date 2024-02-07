2f3e5d61400419cfc9d385b8d41477211c6a6237
kingstar <admin@baks.dev>
2024-02-08 02:09:48 +0300

    Baks Development

--- Type/GenderType.php
+++ Type/GenderType.php
@@ -4,15 +4,16 @@ namespace BaksDev\Reference\Gender\Type;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\StringType;
+use Doctrine\DBAL\Types\Type;
 
-final class GenderType extends StringType
+final class GenderType extends Type
 {
-	public function convertToDatabaseValue($value, AbstractPlatform $platform): mixed
+	public function convertToDatabaseValue($value, AbstractPlatform $platform): string
 	{
 		return (string) $value;
 	}
 
-	public function convertToPHPValue($value, AbstractPlatform $platform): mixed
+	public function convertToPHPValue($value, AbstractPlatform $platform): ?Gender
 	{
         return !empty($value) ? new Gender($value) : null;
 	}
@@ -28,5 +29,10 @@ final class GenderType extends StringType
 	{
 		return true;
 	}
+
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
+    {
+        return $platform->getStringTypeDeclarationSQL($column);
+    }
 	
 }
